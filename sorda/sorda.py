2b2a8d13686587b029b4a560c777e9f3fa083415
Johann Lee <me@qinka.pro>
2021-08-23 09:57:29 +0800

    Fix tpo in sorda.py:32

--- sorda/sorda.py
+++ sorda/sorda.py
@@ -29,7 +29,7 @@ class Sorda:
             with open(update_file, 'r') as f:
                 updates = yaml.load_all(f.read(), Loader=yaml.FullLoader)
             for update in updates:
-                gen: UpdaterBase = self._gen(update)
+                gen: UpdaterBase = self._gens(update)
                 gen.load_origin(config)
                 for config in gen:
                     self.do(config, args, kwargs)
