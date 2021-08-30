8718cd96bbe2cb05bbb81213a76e52c4dc6c372f
Johann Lee <me@qinka.pro>
2021-08-30 17:49:24 +0800

    fix error in sorda.py line 46

--- sorda/sorda.py
+++ sorda/sorda.py
@@ -42,7 +42,7 @@ class Sorda:
             with open(update_file, 'r') as f:
                 updates = yaml.load_all(f.read(), Loader=yaml.FullLoader)
             for update in updates:
-                gen: UpdaterBase = self._gens(update)
+                gen: UpdaterBase = self._gens(update)[0]
                 gen.load_origin(config)
                 for config in gen:
                     self.do(config, args, kwargs)
